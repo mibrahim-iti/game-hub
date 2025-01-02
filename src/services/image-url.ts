3c9a4a7ba9be15dda1f21ef3ec621d5b75922307
Mohamed Ibrahim <mohamed.ibrahim@consultant.volvo.com>
2025-01-02 17:18:22 +0100

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
   if (!url) {
-    return "";
+    return noImage;
   }
 
   const target = "media/";
