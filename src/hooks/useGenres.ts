d9555301a62b10bf2fb40b87336f764bc0b498bf
Mohamed Ibrahim <mohamed.ibrahim@consultant.volvo.com>
2025-01-04 00:18:10 +0100

    Fetch genres with react-query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 export interface Genre {
   id: number;
@@ -6,14 +9,14 @@ export interface Genre {
   image_background: string;
 }
 
-const mapToGenre = (genre: Genre): Genre => ({
-  id: genre.id,
-  name: genre.name,
-  image_background: genre.image_background,
-});
-
 // const useGenres = () => useData<Genre>("/genres"); // this line will call http get genres to fetch it, but we load it static in the next line
 
-const useGenres = () => ({ data: genres.results.map(mapToGenre), error: null, isLoading: false });
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () => apiClient.get<FetchResponse<Genre>>("genres").then((response) => response.data),
+    staleTime: 1000 * 60 * 60 * 24, // 24h
+    initialData: { count: genres.results.length, results: genres.results },
+  });
 
 export default useGenres;
