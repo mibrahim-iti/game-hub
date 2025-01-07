b37bcfe0b9cccfafb2391b5f52468cc219264cea
Mohamed Ibrahim <mohamed.ibrahim@consultant.volvo.com>
2025-01-07 23:05:39 +0100

    Refactor: Move entity interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,10 +1,10 @@
-import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid, FaNeos } from "react-icons/fa";
-import { MdPhoneIphone } from "react-icons/md";
-import { SiNintendo } from "react-icons/si";
-import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
-import { Platform } from "../hooks/usePlatforms";
+import { BsGlobe } from "react-icons/bs";
+import { FaAndroid, FaApple, FaLinux, FaNeos, FaPlaystation, FaWindows, FaXbox } from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { Platform } from "../entities/Platform";
 
 interface Props {
   platforms: Platform[];
